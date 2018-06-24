cff25f73896e64cfd5a4f9f54313f8b930153626
chenshenhai <cshenhai@gmail.com>
2018-06-24 11:44:25 +0800

    feat: update demo 01-02

--- demo/chapter-01-02/build/rollup.config.js
+++ demo/chapter-01-02/build/rollup.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const buble = require('rollup-plugin-buble');
 const babel = require('rollup-plugin-babel');
-const resolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
 
 const resolveFile = function(filePath) {
   return path.join(__dirname, '..', filePath)
@@ -12,56 +10,39 @@ module.exports = {
   input: resolveFile('src/index.js'),
   output: {
     file: resolveFile('dist/index.js'),
-    format: 'cjs',
-    sourcemap: true,
-  },
-  external: [ 
-    // 'babel-runtime/regenerator',
-    // 'babel-runtime/helpers/asyncToGenerator',
-    // 'babel-runtime/helpers/extends',
-  ],
+    format: 'umd',
+  }, 
   plugins: [
-    // babel({
-    //   "presets": [
-    //     ["env", {
-    //       "modules": false
-    //     }],
-    //   ],
-    //   "plugins": [
-    //     "external-helpers",
-    //     "transform-object-rest-spread",
-    //     "transform-es2015-arrow-functions",
-    //     ["transform-runtime", {
-    //       "polyfill": true,
-    //       "regenerator": true
-    //     }],
-    //   ],
-    //   "runtimeHelpers": true,
-    // }),
-    
-    resolve({
-      jsnext: true,
-      main: true
-    }),
-    // commonjs({
-    //   include: 'node_modules/**',
-    // }),
     babel({
-      "exclude": 'node_modules/**',
       "presets": [
-        [
-          "env",
-          {
-            "modules": false
-          }
-        ],
-        "stage-2",
-        "stage-3"
+        ["env", {
+          "modules": false
+        }],
       ],
       "plugins": [
         "external-helpers",
-      ], 
+        "transform-object-rest-spread",
+        "transform-es2015-arrow-functions"
+      ],
     }),
+     
+    // babel({
+    //   "exclude": 'node_modules/**',
+    //   "presets": [
+    //     [
+    //       "env",
+    //       {
+    //         "modules": false
+    //       }
+    //     ],
+    //     "stage-2",
+    //     "stage-3"
+    //   ],
+    //   "plugins": [
+    //     "external-helpers",
+    //   ], 
+    // }),
+
     buble(),
   ],
 }
