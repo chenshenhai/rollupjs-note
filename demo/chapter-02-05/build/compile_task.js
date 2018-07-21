f872f4ca0478d03fe2b0138dea49bed2bd51c8c4
chenshenhai <cshenhai@gmail.com>
2018-07-21 22:07:26 +0800

    feat: update chapter-02-05

---
+++ demo/chapter-02-05/build/compile_task.js
@@ -0,0 +1,38 @@
+const rollup = require('rollup');
+const compose = require('koa-compose');
+
+module.exports = compileTask;
+
+function compileTask(configList){
+  const taskList = [];
+
+  configList.forEach(function(config){
+    taskList.push(wrapTask(config));
+  });
+  
+  compose(taskList)( ).then(function(){
+    console.log('[SUCCESS]: END')
+  }).catch(function(err){
+    console.log(err);
+  })
+}
+
+function wrapTask( config ) {
+  const inputOptions = config;
+  const outputOptions = config.output;
+  return async function(ctx, next) {
+    // create a bundle
+    const bundle = await rollup.rollup(inputOptions);
+
+    console.log(`[INFO] 开始编译 ${inputOptions.input}`);  
+    await bundle.generate(outputOptions);
+    // or write the bundle to disk
+    await bundle.write(outputOptions);
+    console.log(`[INFO] 编译结束 ${outputOptions.file}`);  
+
+    await next();
+  }
+}
+
+
+
