66f03316dff457138f03d131d48e6c107f65daec
chenshenhai <cshenhai@gmail.com>
2018-07-22 17:21:50 +0800

    feat: add chapter 04-01

---
+++ demo/chapter-04-01/build/rollup.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const buble = require('rollup-plugin-buble');
+const babel = require('rollup-plugin-babel');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const nodeGlobals = require('rollup-plugin-node-globals');
+
+const resolveFile = function(filePath) {
+  return path.join(__dirname, '..', filePath)
+}
+
+const babelOptions = {
+  "presets": [
+    ["env", {
+      "modules": false
+    }],
+  ],
+  "plugins": [
+    "external-helpers",
+    "transform-object-rest-spread",
+    "transform-es2015-arrow-functions"
+  ],
+}
+
+module.exports = [
+  {
+    input: resolveFile('src/index.js'),
+    output: {
+      file: resolveFile('dist/index.js'),
+      format: 'umd',
+    }, 
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      nodeGlobals(),
+      babel(babelOptions),
+      buble(),
+    ],
+  },
+]
---
+++ demo/chapter-04-01/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/cjs/react-dom.production.min';
+// import ReactDOM from 'react-dom/umd/react-dom.production.min';
+
+// console.log(React)
+console.log(React, ReactDOM)
+
+
