632dc74cdc316a3f6e254ff6ae84de6196f916d6
chenshenhai <cshenhai@gmail.com>
2018-07-24 22:16:13 +0800

    feat: update

--- demo/chapter-04-01/src/index.js
+++ demo/chapter-04-01/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/cjs/react-dom.production.min';
-// import ReactDOM from 'react-dom/umd/react-dom.production.min';
+import List from './component/List.jsx';
 
-// console.log(React)
-console.log(React, ReactDOM)
+const dataList = [
+  { name: 'hello' },
+  { name: 'world' },
+  { name: 'react' },
+  { name: 'react-dom' },
+]
 
 
+ReactDOM.render(
+  React.createElement(List, { dataList }, null), 
+  document.getElementById('App')
+);
