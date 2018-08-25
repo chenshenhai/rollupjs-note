cfdb3da64fba1cb2696e3e4d065281c86f0e5752
chenshenhai <cshenhai@gmail.com>
2018-08-25 17:09:49 +0800

    feat: update chapter-02-02-04

---
+++ demo/chapter-02-02-04/build/rollup.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const babel = require('rollup-plugin-babel');
+
+const resolveFile = function(filePath) {
+  return path.join(__dirname, '..', filePath)
+}
+
+const babelOptions = {
+  "presets": [
+    ["env", {
+      "modules": false
+    }],
+  ],
+  "plugins": [
+    "transform-object-rest-spread",
+  ],
+}
+
+module.exports = [ 
+  {
+    input: resolveFile('src/index.js'),
+    output: {
+      file: resolveFile('dist/index.js'),
+      format: 'iife',
+      name: 'Demo',
+      amd: {
+        id: 'lib/demo'
+      },
+    }, 
+    plugins: [
+      babel(babelOptions),
+    ],
+  },
+
+]
