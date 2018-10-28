b944e8f729eef1d2c1a9622bc4bf4b2e7bcd3565
chenshenhai <cshenhai@gmail.com>
2018-10-28 14:52:34 +0800

    feat: update chapter-03-02

--- demo/chapter-03-02/build/rollup.config.js
+++ demo/chapter-03-02/build/rollup.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const buble = require('rollup-plugin-buble');
 const babel = require('rollup-plugin-babel');
 const postcss = require('rollup-plugin-postcss');
 const sass = require('node-sass');
@@ -17,9 +16,7 @@ const babelOptions = {
     }],
   ],
   "plugins": [
-    "external-helpers",
     "transform-object-rest-spread",
-    "transform-es2015-arrow-functions"
   ],
 }
 
@@ -52,7 +49,6 @@ module.exports = [
         process: processSass,
       }),
       babel(babelOptions),
-      buble(),
     ],
   },
 ]
