0edcefb8384f0810f1b7e3b7a3b5ec26be53b5bc
chenshenhai <cshenhai@gmail.com>
2018-10-28 15:38:05 +0800

    feat: clean chapter

--- demo/chapter-03-04/build/build.js
+++
@@ -1,4 +0,0 @@
-const compileTask = require('./compile_task');
-const configList = require('./rollup.config.prod');
-
-compileTask(configList)
--- demo/chapter-03-04/src/index.js
+++
@@ -1,8 +0,0 @@
-import './css/index.scss';
-
-const arr1 = [1,2,3];
-const arr2 = [4,5,6];
-console.log([...arr1, ...arr2]);
-
-
-
