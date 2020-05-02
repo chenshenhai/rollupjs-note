76ff2413d4cd515c2b1189f60d1ada112ac5543e
chenshenhai <cshenhai@gmail.com>
2020-05-02 16:57:05 +0800

    feat: upgrade chapter-02/04

--- demo/chapter-02-04/build/rollup.config.js
+++ demo/chapter-02-04/build/rollup.config.js
@@ -1,22 +1,14 @@
 const path = require('path');
-const buble = require('rollup-plugin-buble');
-const babel = require('rollup-plugin-babel');
-const nodeResolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
+const { babel } = require('@rollup/plugin-babel');
+const nodeResolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
 
 const resolveFile = function(filePath) {
   return path.join(__dirname, '..', filePath)
 }
 
 const babelOptions = {
-  "presets": [
-    ["env", {
-      "modules": false
-    }],
-  ],
-  "plugins": [
-    "transform-object-rest-spread",
-  ],
+  "presets": ['@babel/preset-env'],
 }
 
 module.exports = [
@@ -30,7 +22,6 @@ module.exports = [
       nodeResolve(),
       commonjs(),
       babel(babelOptions),
-      buble(),
     ],
   },
 ]
